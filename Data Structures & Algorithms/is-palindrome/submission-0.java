76b82f9cafa7c7d953c1d0c7bd180a1d6443c2ad
Deenah Sheikh <deenahsheikh20@gmail.com>
2026-07-06 23:17:46 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left=0;
+        int right=s.length()-1;
+        while(right>left){
+            if(!Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+                continue;
+            }
+            if(Character.toLowerCase(s.charAt(left))!=(Character.toLowerCase(s.charAt(right)))){
+                return false;
+            }
+            right--;
+            left++;
+        }        
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
